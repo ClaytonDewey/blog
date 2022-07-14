a5ca219b623aa0f601ea08cd88f74f5bcaec4d53
ClaytonDewey <clay@dryadmedia.com>
2022-07-14 07:52:25 -0500

    Finish fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log("About to fetch posts");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log("fetched posts!");
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
